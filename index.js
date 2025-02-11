6e1c6c64b366562d4b9c80e04966cd29cddfcf81
matiur2120 <matiur211993@gmail.com>
2025-02-11 23:57:33 +0600

    Update server port

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(cors({
 app.use(cookieParser())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}))
-const port = process.env.SERVER_PORT
+const port = process.env.PORT || 5000;
 app.use('/api', router)
 app.get('/', (req, res)=>{
     res.json({
